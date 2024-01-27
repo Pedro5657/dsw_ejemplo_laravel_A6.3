3de0d219c4c2d2db3953f10f0088614c8fe75cb0
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-01-27 23:18:02 +0100

    Controller, view and route programmed. Can't store the controller inside Admin folder.

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.admin")
+@section("title","Panel de control")
+@section("content")
+<div class="card">
+  <div class="card-header">
+    Página principal del panel de control
+  </div>
+  <div class="card-body">
+    Bienvenido/a al panel de control. Utiliza el menú lateral para navegar entre las diferentes opciones.
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', "App\Http\Controllers\HomeController@index")->name("home.index")
 Route::get('/products',"App\Http\Controllers\ProductController@index")->name("product.index");
 Route::get('/products/{id}',"App\Http\Controllers\ProductController@show")->name("product.show");
 Route::get('/about', "App\Http\Controllers\HomeController@about")->name("home.about");
+Route::get('/admin',"App\Http\Controllers\AdminHomeController@index")->name("admin.home.index");
